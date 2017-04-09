4a6aa42caa7729d04ac95695baf378e61cf2d247
cdelfattore <cdelfattore@live.com>
2017-04-09 17:38:35 -0400

    Added jUnit test methods

---
+++ FoodStoreModelTest/src/foodstore/tests/FoodStoreFacadeTest.java
@@ -0,0 +1,54 @@
+package foodstore.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FoodStoreFacadeTest {
+
+	@Test
+	public void testGetEmployee() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCreateEmployee() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCurrentlyEmployed() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCreateItem() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetItem() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetItemDeparment() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCreateStore() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetStore() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetStoreName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
